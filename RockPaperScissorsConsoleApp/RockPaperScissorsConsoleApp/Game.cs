d023337e528fabb164bbf8d421faf2cc41e300be
ACKAdeyemi <akola-adeyemi@spartaglobal.com>
2018-08-22 09:11:53 +0100

    Game complete.

--- RockPaperScissorsConsoleApp/RockPaperScissorsConsoleApp/Game.cs
+++ RockPaperScissorsConsoleApp/RockPaperScissorsConsoleApp/Game.cs
@@ -5,112 +5,113 @@ namespace RockPaperScissorsConsoleApp
     public class Game
     {
         private bool keepPlaying = true;
-        private string name;
-        private string chosenMove;
 
         public Game()
         {
             StartGame();
         }
 
-        public void StartGame()
+        private void StartGame()
         {
-            Player player = new Player(name, chosenMove);
-            Console.WriteLine("Welcome to RockPaperScissors! What is your name?");
-            player.name = Console.ReadLine();
-            Console.WriteLine("\n\nAhh thank you " + player.name + ", let's start the game.");
-            Console.WriteLine("\n\nRULES: You get to choose from 3 options - RockPaperScissors.");
+            Player player = new Player();
+            Player computer = new Player
+            {
+                name = "Computer"
+            };
+
+            Welcome(computer, player);
 
             while (keepPlaying)
             {
-                Console.WriteLine("Do you choose rock,paper or scissors");
-                string userChoice = Console.ReadLine();
+                Console.WriteLine("Do you choose (1) ROCK, (2) PAPER or (3) SCISSORS");
+                string entry = Console.ReadLine();
+                Int32.TryParse(entry, out player.chosenMove);
 
                 Random r = new Random();
-                int computerChoice = r.Next(1, 4);
+                computer.chosenMove = r.Next(1, 4);
 
-                if (computerChoice == 1)
-                {
-                    if (userChoice == "rock")
-                    {
-                        Console.WriteLine("The computer chose rock");
-                        Console.WriteLine("It is a tie ");
-                    }
-                    else if (userChoice == "paper")
-                    {
-                        Console.WriteLine("The computer chose paper");
-                        Console.WriteLine("It is a tie ");
-
-                    }
-                    else if (userChoice == "scissors")
-                    {
-                        Console.WriteLine("The computer chose scissors");
-                        Console.WriteLine("It is a tie ");
-                    }
-                    else
-                    {
-                        Console.WriteLine("You must choose rock,paper or scissors!");
-
-                    }
-
-                }
-
-                else if (computerChoice == 2)
-                {
-                    if (userChoice == "rock")
-                    {
-                        Console.WriteLine("The computer chose paper");
-                        Console.WriteLine("Sorry you lose,paper beat rock");
-
-                    }
-                    else if (userChoice == "paper")
-                    {
-                        Console.WriteLine("The computer chose scissors");
-                        Console.WriteLine("Sorry you lose,scissors beat paper ");
-
-                    }
-                    else if (userChoice == "scissors")
-                    {
-                        Console.WriteLine("The computer chose rock");
-                        Console.WriteLine("Sorry you lose,rock beats scissors");
-                    }
-                    else
-                    {
-                        Console.WriteLine("You must choose rock,paper or scissors!");
-                    }
-                }
-                else if (computerChoice == 3)
-                {
-                    if (userChoice == "rock")
-                    {
-                        Console.WriteLine("The computer chose scissors");
-                        Console.WriteLine("You win,rock beats scissors");
-
-                    }
-                    else if (userChoice == "paper")
-                    {
-                        Console.WriteLine("The computer chose rock");
-                        Console.WriteLine("You win,paper beats rock");
-
-                    }
-                    else if (userChoice == "scissors")
-                    {
-                        Console.WriteLine("The computer chose paper");
-                        Console.WriteLine("You win,scissors beat paper");
-
-                    }
-                    else
-                    {
-                        Console.WriteLine("You must choose rock,paper or scissors!");
-
-                    }
-
-                }
-
-                Console.WriteLine("New game? y/n");
-                ConsoleKeyInfo cki = Console.ReadKey(); //wait for player to press a key
-                keepPlaying = cki.KeyChar == 'y'; //continue only if y was pressed
+                CheckWin(computer, player);
+                ChooseToReplay();
             }
+        }
+
+        private void Welcome(Player computer, Player player)
+        {
+            Console.WriteLine("Welcome to RockPaperScissors: The Ultimate Console Edition! \nWhat is your name?");
+            player.name = Console.ReadLine();
+            Console.WriteLine("\nAhh thank you " + player.name + ", let's start the game.");
+            Console.WriteLine("\nRULES: You get to choose 1 move from 3 moves - ROCK, PAPER or SCISSORS. \nROCK beats SCISSORS, SCISSORS beats PAPER, PAPER beats ROCK. \nYou (" + player.name + ") vs " + computer.name + "\nType the NUMBER related to the move you want to use and press ENTER. \nGood luck...");
+        }
+
+        private void CheckWin(Player computer, Player player)
+        {
+            if (computer.chosenMove == 1)
+            {
+                switch (player.chosenMove)
+                {
+                    case 1:
+                        Console.WriteLine("The computer chose ROCK. \nIt is a tie!.");
+                        break;
+
+                    case 2:
+                        Console.WriteLine("The computer chose ROCK. \nYou win, PAPER beats ROCK!");
+                        break;
+
+                    case 3:
+                        Console.WriteLine("The computer chose ROCK. \nSorry you lose,ROCK beats SCISSORS!");
+                        break;
+
+                    default:
+                        Console.WriteLine("You must enter (1) for ROCK, (2) for PAPER or (3) for SCISSORS!");
+                        break;
+                }
+
+            }
+
+            else if (computer.chosenMove == 2)
+            {
+                switch (player.chosenMove)
+                {
+                    case 1:
+                        Console.WriteLine("The computer chose PAPER. \nSorry you lose, PAPER beat ROCK!");
+                        break;
+
+                    case 2:
+                        Console.WriteLine("The computer chose PAPER. \nIt is a tie!.");
+                        break;
+                    case 3:
+                        Console.WriteLine("The computer chose PAPER. \nYou win, SCISSORS beats PAPER!");
+                        break;
+                    default:
+                        Console.WriteLine("You must enter (1) for ROCK, (2) for PAPER or (3) for SCISSORS!");
+                        break;
+                }
+            }
+            else if (computer.chosenMove == 3)
+            {
+                switch (player.chosenMove)
+                {
+                    case 1:
+                        Console.WriteLine("The computer chose SCISSORS. \nYou win, ROCK beats SCISSORS!");
+                        break;
+                    case 2:
+                        Console.WriteLine("The computer chose SCISSORS. \nSorry you lose, SCISSORS beats PAPER!");
+                        break;
+                    case 3:
+                        Console.WriteLine("The computer chose SCISSORS. \nIt is a tie!");
+                        break;
+                    default:
+                        Console.WriteLine("You must enter (1) for ROCK, (2) for PAPER or (3) for SCISSORS!");
+                        break;
+                }
+            }
+        }
+
+        private void ChooseToReplay()
+        {
+            Console.WriteLine("New game? y/n");
+            ConsoleKeyInfo cki = Console.ReadKey(); //wait for player to press a key
+            keepPlaying = cki.KeyChar == 'y'; //returns bool, continue only if y was pressed
         }
     }
 
--- RockPaperScissorsConsoleApp/RockPaperScissorsConsoleApp/Player.cs
+++ RockPaperScissorsConsoleApp/RockPaperScissorsConsoleApp/Player.cs
@@ -1,16 +1,8 @@
-﻿using System;
-
-namespace RockPaperScissorsConsoleApp
+﻿namespace RockPaperScissorsConsoleApp
 {
     public class Player
     {
         public string name;
-        public string chosenMove;
-
-        public Player(string _name, string _chosenMove)
-        {
-            name = _name;
-            chosenMove = _chosenMove;
-        }
+        public int chosenMove;
     }
 }
